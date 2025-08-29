5ac0e3da679574cb0213b659df41526c79f00c8f
Seb Master <elisalawyer@gmail.com>
2025-08-29 14:08:12 +0600

    Update PaymentInfo.js

--- components/order-form/PaymentInfo.js
+++ components/order-form/PaymentInfo.js
@@ -21,6 +21,7 @@ export default function PaymentInfo({
   bankDetails,
 }) {
   const [privacyOpen, setPrivacyOpen] = useState(false);
+  const [agreedToTerms, setAgreedToTerms] = useState(false);
   const openPrivacy = useCallback(() => setPrivacyOpen(true), []);
   const closePrivacy = useCallback(() => setPrivacyOpen(false), []);
 
@@ -37,7 +38,7 @@ export default function PaymentInfo({
       className="bg-white p-8 rounded-2xl shadow-lg border border-orange-100"
     >
       <div className="mb-8">
-        <h2 className="text-2xl lg:text-3xl font-bold text-gray-900 mb-2 flex items-center gap-3">
+        <h2 className="text-3xl font-bold text-gray-900 mb-2 flex items-center gap-3">
           <span className="bg-orange-100 text-orange rounded-full w-10 h-10 flex items-center justify-center">
             3
           </span>
@@ -85,6 +86,11 @@ export default function PaymentInfo({
             <span className="hidden sm:inline text-sm">View</span>
           </button>
         </div>
+         <Checkbox
+           label="I agree to the Terms & Conditions and Privacy Statement *"
+           checked={agreedToTerms}
+           onChange={setAgreedToTerms}
+        />
 
         <BankDetails details={bankDetails} />
 
@@ -117,7 +123,8 @@ export default function PaymentInfo({
           type="submit"
           disabled={
             isSubmitting ||
-            (!formData.payment.receipt && !formData.payment.skipReceipt)
+             (!formData.payment.receipt && !formData.payment.skipReceipt) ||
++            !agreedToTerms
           }
           isLoading={isSubmitting}
           icon={<ArrowRightIcon className="w-4 h-4" />}
@@ -151,175 +158,95 @@ export default function PaymentInfo({
 
               {/* Scrollable content */}
               <div className="max-h-[70vh] overflow-y-auto pr-1 sm:pr-2 text-xs sm:text-sm leading-relaxed space-y-3">
-                <p>
-                  This online privacy statement describes how Dinar Exchange
-                  collects and uses your personal information
-                  (&quot;Information&quot;) that we obtain when you visit and/or
-                  use our Web site. The currency exchange services offered on
-                  this Web site are provided by Oz Trading Group Pty Ltd ACN 158
-                  981 787 trading as Dinar Exchange. As a reporting entity Dinar
-                  Exchange is bound by the National Privacy Principles under the
-                  Privacy Act 1988 (Commonwealth).
-                </p>
+      <p>
+        This online privacy statement describes how Dinar Exchange collects and uses your personal information (&quot;Information&quot;) that we obtain when you visit and/or use our Web site. The currency exchange services offered on this Web site are provided by Oz Trading Group Pty Ltd ACN 158 981 787 trading as Dinar Exchange. As a reporting entity Dinar Exchange is bound by the National Privacy Principles under the Privacy Act 1988 (Commonwealth).
+      </p>
 
-                <h5 className="font-semibold">WHAT DO WE COLLECT</h5>
-                <p>
-                  Dinar Exchange collects Information about you from a variety
-                  of sources including those listed below. By exchanging
-                  currency through Dinar Exchange you are consenting to the
-                  collection, transfer and storage of this Information by
-                  computers or other transfer or storage devices in Australia,
-                  including the transfer and retention of your data outside of
-                  the Australia to other countries. We currently do not transfer
-                  your information to any other country and we do not use any
-                  cloud computing service to back up our data.
-                </p>
+      <h5 className="font-semibold">WHAT DO WE COLLECT</h5>
+      <p>
+        Dinar Exchange collects Information about you from a variety of sources including those listed below. By exchanging currency through Dinar Exchange you are consenting to the collection, transfer and storage of this Information by computers or other transfer or storage devices in Australia, including the transfer and retention of your data outside of the Australia to other countries. We currently do not transfer your information to any other country and we do not use any cloud computing service to back up our data.
+      </p>
 
-                <ul className="list-disc pl-5 space-y-1">
-                  <li>
-                    We collect Information you supply when: (a) you ask us for
-                    currency exchange; (b) you submit Information on
-                    applications or other forms to us; or (c) you otherwise
-                    submit personal information to us;
-                  </li>
-                  <li>
-                    We collect personal information about your transactions with
-                    us;
-                  </li>
-                  <li>
-                    We collect personal information about you from a variety of
-                    third party sources such as our business customers,
-                    government agencies and consumer reporting agencies and
-                    other suppliers of public information, in order to verify
-                    any of the information you provide in order to effect your
-                    transactions;
-                  </li>
-                  <li>
-                    We collect and retain your bank card details for use in
-                    future transactions; and
-                  </li>
-                  <li>
-                    We also collect Information on your online activity as
-                    described in the paragraph below titled &quot;INTERNET
-                    TECHNOLOGY.&quot;
-                  </li>
-                </ul>
+      <ul className="list-disc pl-5 space-y-1">
+        <li>
+          We collect Information you supply when: (a) you ask us for currency exchange; (b) you submit Information on applications or other forms to us; or (c) you otherwise submit personal information to us;
+        </li>
+        <li>
+          We collect personal information about your transactions with us;
+        </li>
+        <li>
+          We collect personal information about you from a variety of third party sources such as our business customers, government agencies and consumer reporting agencies and other suppliers of public information, in order to verify any of the information you provide in order to effect your transactions;
+        </li>
+        <li>
+          We collect and retain your bank card details for use in future transactions; and
+        </li>
+        <li>
+          We also collect Information on your online activity as described in the paragraph below titled &quot;INTERNET TECHNOLOGY.&quot;
+        </li>
+      </ul>
 
-                <h5 className="font-semibold">INTERNET TECHNOLOGY</h5>
-                <p>
-                  We do use internet technologies like cookies and web beacons
-                  for a variety of purposes, including, but not limited to,
-                  those identified below.
-                </p>
-                <ul className="list-disc pl-5 space-y-1">
-                  <li>To assist us in providing services to you;</li>
-                  <li>
-                    To allow you to change web pages during your visit without
-                    having to re-enter your password;
-                  </li>
-                  <li>
-                    To store your preferences and other information and to track
-                    activity on our website;
-                  </li>
-                  <li>
-                    To better understand the effectiveness of our promotional
-                    campaigns; and
-                  </li>
-                  <li>
-                    To determine whether you&apos;ve acted on our promotional
-                    messages.
-                  </li>
-                </ul>
-                <p>
-                  A &quot;cookie&quot; is a text file placed on your
-                  computer&apos;s hard drive by a web site. Your web browser
-                  uses it to guide you to sites based on your interest.
-                  &quot;Web beacons&quot; are transparent electronic images
-                  placed in the web code that collect non-personal data while
-                  visiting a Web site. You are able to disable cookies and web
-                  beacons by changing your browser preferences. Please
-                  understand that this will limit the performance of Dinar
-                  Exchange&apos;s Web site. Your browser usually has
-                  documentation on how to disable cookies and Web beacons.
-                </p>
+      <h5 className="font-semibold">INTERNET TECHNOLOGY</h5>
+      <p>
+        We do use internet technologies like cookies and web beacons for a variety of purposes, including, but not limited to, those identified below.
+      </p>
+      <ul className="list-disc pl-5 space-y-1">
+        <li>To assist us in providing services to you;</li>
+        <li>
+          To allow you to change web pages during your visit without having to re-enter your password;
+        </li>
+        <li>
+          To store your preferences and other information and to track activity on our website;
+        </li>
+        <li>
+          To better understand the effectiveness of our promotional campaigns; and
+        </li>
+        <li>
+          To determine whether you&apos;ve acted on our promotional messages.
+        </li>
+      </ul>
+      <p>
+        A &quot;cookie&quot; is a text file placed on your computer&apos;s hard drive by a web site. Your web browser uses it to guide you to sites based on your interest. &quot;Web beacons&quot; are transparent electronic images placed in the web code that collect non-personal data while visiting a Web site. You are able to disable cookies and web beacons by changing your browser preferences. Please understand that this will limit the performance of Dinar Exchange&apos;s Web site. Your browser usually has documentation on how to disable cookies and Web beacons.
+      </p>
 
-                <h5 className="font-semibold">INFORMATION WE DISCLOSE</h5>
-                <p>
-                  Subject to your consent or as otherwise permitted by law, we
-                  may disclose the Information we collect as described below.
-                </p>
-                <ul className="list-disc pl-5 space-y-1">
-                  <li>
-                    Legal, regulatory, security, processing and Australian and
-                    foreign government requirements, applicable to us or our
-                    Affiliates or service providers; and
-                  </li>
-                  <li>As permitted or required by law.</li>
-                </ul>
+      <h5 className="font-semibold">INFORMATION WE DISCLOSE</h5>
+      <p>
+        Subject to your consent or as otherwise permitted by law, we may disclose the Information we collect as described below.
+      </p>
+      <ul className="list-disc pl-5 space-y-1">
+        <li>
+          Legal, regulatory, security, processing and Australian and foreign government requirements, applicable to us or our Affiliates or service providers; and
+        </li>
+        <li>As permitted or required by law.</li>
+      </ul>
 
-                <h5 className="font-semibold">CREDIT AGENCY INFORMATION</h5>
-                <p>
-                  We use an electronic verification service that polls credit
-                  reporting agencies for the headline information (not your
-                  credit status or credit application history) as part of our
-                  electronic verification process. By headline information we
-                  mean name, date of birth, current and previous addresses. No
-                  credit information is accessed. Your express consent is
-                  required for this process. Otherwise you can send us original
-                  certified copies by mail of your identification documentation.
-                </p>
+      <h5 className="font-semibold">CREDIT AGENCY INFORMATION</h5>
+      <p>
+        We use an electronic verification service that polls credit reporting agencies for the headline information (not your credit status or credit application history) as part of our electronic verification process. By headline information we mean name, date of birth, current and previous addresses. No credit information is accessed. Your express consent is required for this process. Otherwise you can send us original certified copies by mail of your identification documentation.
+      </p>
 
-                <h5 className="font-semibold">CONFIDENTIALITY AND SECURITY</h5>
-                <p>
-                  We endeavour to maintain physical, electrical and procedural
-                  safeguards to guard your Information. Despite our efforts
-                  third parties may unlawfully intercept or access transmissions
-                  sent to us or may wrongly instruct you to disclose personal
-                  information to them while posing as Dinar Exchange. Some
-                  disclosures are not subject to your consent. These include,
-                  among others, disclosures necessary to effect, administer or
-                  enforce a transaction you request; disclosures as permitted or
-                  required by law; for public safety; to prevent fraud; or in
-                  connection with other illegal activities.
-                </p>
-                <p>
-                  Dinar Exchange maintains your choice and transaction history
-                  based on our record retention policies. If you do not perform
-                  another transaction within the retention time period, your
-                  Information, as well as your choice will be removed.
-                </p>
+      <h5 className="font-semibold">CONFIDENTIALITY AND SECURITY</h5>
+      <p>
+        We endeavour to maintain physical, electrical and procedural safeguards to guard your Information. Despite our efforts third parties may unlawfully intercept or access transmissions sent to us or may wrongly instruct you to disclose personal information to them while posing as Dinar Exchange. Some disclosures are not subject to your consent. These include, among others, disclosures necessary to effect, administer or enforce a transaction you request; disclosures as permitted or required by law; for public safety; to prevent fraud; or in connection with other illegal activities.
+      </p>
+      <p>
+        Dinar Exchange maintains your choice and transaction history based on our record retention policies. If you do not perform another transaction within the retention time period, your Information, as well as your choice will be removed.
+      </p>
 
-                <h5 className="font-semibold">ACCURACY OF INFORMATION</h5>
-                <p>
-                  You may request (by contacting us in the manner set out below)
-                  details of the personal information we hold and request us to
-                  correct or erase any erroneous or out-of-date information. We
-                  reserve the right to independently verify claims made. To
-                  protect your privacy, we also will take reasonable steps to
-                  verify your identity before providing details or corrections.
-                </p>
+      <h5 className="font-semibold">ACCURACY OF INFORMATION</h5>
+      <p>
+        You may request (by contacting us in the manner set out below) details of the personal information we hold and request us to correct or erase any erroneous or out-of-date information. We reserve the right to independently verify claims made. To protect your privacy, we also will take reasonable steps to verify your identity before providing details or corrections.
+      </p>
 
-                <h5 className="font-semibold">EXTERNAL WEB SITES</h5>
-                <p>
-                  Dinar Exchange&apos;s web site may be linked to or from third
-                  party Web sites. Dinar Exchange is not responsible for the
-                  content or privacy practices employed by web sites that are
-                  linked to or from our web site.
-                </p>
+      <h5 className="font-semibold">EXTERNAL WEB SITES</h5>
+      <p>
+        Dinar Exchange&apos;s web site may be linked to or from third party Web sites. Dinar Exchange is not responsible for the content or privacy practices employed by web sites that are linked to or from our web site.
+      </p>
 
-                <h5 className="font-semibold">CHANGES</h5>
-                <p>
-                  Dinar Exchange reserves the right to modify this Privacy
-                  Statement. Updated Privacy Statements will be posted at this
-                  Web site when amendments occur. We urge you to review this
-                  Privacy Statement when you visit to obtain the most current
-                  statement. You may change your choices at any time. We will be
-                  changing this statement in 2014 to conform with the
-                  requirements of the new Australian Privacy Principles which
-                  commence on 12 March 2014. We accept questions on this policy
-                  by email and by telephone.
-                </p>
-              </div>
+      <h5 className="font-semibold">CHANGES</h5>
+      <p>
+        Dinar Exchange reserves the right to modify this Privacy Statement. Updated Privacy Statements will be posted at this Web site when amendments occur. We urge you to review this Privacy Statement when you visit to obtain the most current statement. You may change your choices at any time. We will be changing this statement in 2014 to conform with the requirements of the new Australian Privacy Principles which commence on 12 March 2014. We accept questions on this policy by email and by telephone.
+      </p>
+    </div>
 
               <div className="mt-5 flex justify-center">
                 <Button onClick={closePrivacy} type="button">
